cf5b146292a525c8260eeb300d997abf7dac1494
sbrinton <YendisFish@YendisFish.YendisFish>
2022-02-27 15:07:49 -0700

    Added constructor to ThreadBuilder.cs

--- Threads/ThreadBuilder.cs
+++ Threads/ThreadBuilder.cs
@@ -3,5 +3,10 @@ namespace SimpleLibraryProject.Threads
     public class ThreadBuilder
     {
         public Action action { get; set; }
+
+        public ThreadBuilder(Action action)
+        {
+            this.action = action;
+        }
     }
 }
