cfe157ea93050fb364e0832e67429450a2adeb9d
sbrinton <YendisFish@YendisFish.YendisFish>
2022-03-09 13:16:24 -0700

    Started on hash for actual password sending over the network

--- Security/Hashing/HashBuilder.cs
+++ Security/Hashing/HashBuilder.cs
@@ -33,5 +33,48 @@ namespace SimpleLibraryProject.Security.Hashing
 
             return new HashOutput(ret, salt);
         }
+
+        public static async Task<HashOutput> QuickSecureHash(string password)
+        {
+            byte[] bsalt = await Security.Hashing.SaltBuilder.BuildSalt();
+            string salt = Encoding.ASCII.GetString(bsalt);
+
+            string basehash = Encoding.ASCII.GetString(await Encryption.HashBuilder.BuildHash512(password, salt));
+
+            string endput = "";
+            for (int i = 0; i < 20000; i++)
+            {
+                string val1 = Encoding.ASCII.GetString(await Encryption.HashBuilder.BuildHash512(password, salt));
+                string val2 = await Encryption.Misc.ReverseSequence(val1);
+                string val3 = Encoding.ASCII.GetString(await Encryption.HashBuilder.BuildHash512(val2, salt));
+                endput = Encoding.ASCII.GetString(await Encryption.HashBuilder.BuildHash512(val3, salt));
+            }
+
+            return new HashOutput(endput, salt);
+        }
+
+        public static async Task<HashOutput> CompletelyDestroyMyDataHash(string password, string salt)
+        {
+            string charhsh = "";
+            foreach (char val in password)
+            {
+                string ch = Encoding.ASCII.GetString(await Encryption.HashBuilder.BuildHash512(val.ToString(), salt));
+                charhsh = charhsh + ch;
+            }
+
+            string rehashed = Encoding.ASCII.GetString(await Encryption.HashBuilder.BuildHash512(charhsh, salt));
+
+            string endout = "";
+            for (int i = 0; i < 100000; i++)
+            {
+                string val1 = Encoding.ASCII.GetString(await Encryption.HashBuilder.BuildHash512(rehashed, salt));
+                string val2 = await Encryption.Misc.ReverseSequence(val1);
+                string val3 = Encoding.ASCII.GetString(await Encryption.HashBuilder.BuildHash512(val2, salt));
+
+                endout = val3;
+            }
+
+            return new HashOutput(endout, salt);
+        }
     }
 }
