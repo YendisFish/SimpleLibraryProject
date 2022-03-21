35216d01f0249ea93b4dfbcda3ab1a06cc164fb9
YendisFish <brintonsid@gmail.com>
2022-03-20 21:05:08 -0600

    Added more sorting algorithms

--- Processing/Sorting/Sorter.cs
+++ Processing/Sorting/Sorter.cs
@@ -1,6 +1,6 @@
 namespace SimpleLibraryProject.Processing.Sorting
 {
-    public class Sorter
+    public class Sorter<T>
     {
         public static async Task<List<DateTime>> OrderListByDate(List<DateTime> list)
         {
@@ -8,6 +8,39 @@ namespace SimpleLibraryProject.Processing.Sorting
             
             List<DateTime> ret = new();
             ret.AddRange(enumerable);
+
+            return ret;
+        }
+
+        public static async Task<List<string>> OrderAlphabetically(List<string> list)
+        {
+            List<string> ret = list.OrderBy(x => x).ToList();
+            return ret;
+        }
+
+        public static async Task<List<T>> OrderByOccurrence(List<T> list)
+        {
+            Dictionary<T, int> count = new();
+            
+            foreach (T val in list)
+            {
+                if (count.ContainsKey(val))
+                {
+                    count[val] = count[val] + 1;
+                } else if (!count.ContainsKey(val))
+                {
+                    count.Add(val, 1);   
+                }
+            }
+
+            IOrderedEnumerable<KeyValuePair<T, int>> sorted = count.OrderByDescending(x => x.Value);
+
+            List<T> ret = new();
+            
+            foreach (KeyValuePair<T, int> val in sorted)
+            {
+                ret.Add(val.Key);
+            }
 
             return ret;
         }
